5ce48dc5271ddba3c09b86e777b4d96dd9633ead
KAL17002493 <KAL17002493@btc.ac.uk>
2022-04-28 14:56:20 +0100

    Add project files.

---
+++ L5A2Programming/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L5A2Programming.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
